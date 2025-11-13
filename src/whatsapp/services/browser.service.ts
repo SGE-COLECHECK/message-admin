2060296def2bc90bfb4ee211f39301d94dfd8446
yerson001 <yhon.sanchez@ucsp.edu.pe>
2025-11-13 01:46:46 -0500

    production

--- src/whatsapp/services/browser.service.ts
+++ src/whatsapp/services/browser.service.ts
@@ -16,8 +16,8 @@ export class BrowserService {
 
     this.logger.log(`🚀 Iniciando navegador para la sesión '${sessionName}' con perfil en ${profilePath}`);
     const browser = await puppeteer.launch({
-      headless: false,
-      //headless: true,
+      //headless: false,
+      headless: true,
       userDataDir: profilePath,
       args: [
         '--no-sandbox',
